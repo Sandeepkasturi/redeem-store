71b606abbb9875b3865ea360ab36462a6fd05447
Sandeepkasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2023-04-14 04:17:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ scripts.js
@@ -0,0 +1,75 @@
+/* Place your JavaScript in this file */
+
+// App 1
+// Online Games
+function downloadApp1() {
+  window.open('https://www.mediafire.com/file/2gbz5l26f98a7x6/OnlineGames.apk/file',"_blank");
+}
+
+// App 2
+//redeemtech
+function downloadApp2() {
+  window.open('https://www.mediafire.com/file/ptz9ymm28mruge7/Redeem_Tech.apk/file',"_blank");
+}
+
+// App 3
+//truth or dare
+function downloadApp3() {
+  window.open('https://www.mediafire.com/file/yg93pxrfdg5z6ad/TruthorDare.apk/file',"_blank");
+}
+
+// App 4
+//html compiler
+function downloadApp4() {
+  window.open('https://www.mediafire.com/file/25aspveylho81gr/HTML-COMPILER.apk/file',"_blank");
+}
+
+// App 5
+//turbo c7 by akki
+function downloadApp5() {
+  window.open('https://www.mediafire.com/file/4pzp375gotwlwyu/TurboC_7_by_Akki.lnk/file',"_blank");
+}
+
+// Friday AI
+function downloadApp6() {
+  window.open('https://www.mediafire.com/file/f4fno9bdlfyc5bm/FRIDAY-DESKTOP-AI.exe/file',"_blank"); 
+}
+
+// App 7
+//kodachi linux
+function downloadApp7() {
+  window.open('https://www.mediafire.com/file/713azkrgnst9fho/kodachi-8.26-64.iso/file',"_blank");
+}
+
+// Rolex AI
+function downloadApp8() {
+  window.open('https://www.mediafire.com/file/inzipjppv9b61ff/ROLEX-AI.exe/file',"_blank");
+}
+// Rolex chat AI
+function downloadApp9() {
+  window.open('https://www.mediafire.com/file/0zxbo2a7ocgl6l0/ROLEX-CHAT.exe/file',"_blank");
+}
+// code search
+function downloadApp10() {
+  window.open('https://www.mediafire.com/file/i6tjy932q25xt0d/Code_Search.apk/file',"_blank");
+}
+// yt downloder 
+function downloadApp11() {
+  window.open('https://www.mediafire.com/file/qkfzyikkhps509h/YT-video-Downloder.exe/file',"_blank");
+}
+// Redeem Store
+function downloadApp12() {
+  window.open('https://www.mediafire.com/file/m430vsgcq7iquze/_Redeem_Store_.apk/file',"_blank");
+}
+// Megha
+function downloadApp13() {
+  window.open('https://www.mediafire.com/file/tcsvhnz6m6apc9a/_Megha_AI_.apk/file',"_blank");
+}
+// 2048 cube winner
+function downloadApp14() {
+  window.open('https://www.mediafire.com/file/vycjer2j4a67vse/_2048_cube_winner_16960459.apk/file',"_blank");
+}
+// code translater
+function downloadApp15() {
+  window.open('https://www.mediafire.com/file/faxaiu3obsnvfjg/_Megha_Code_Translator_.apk/file',"_blank");
+}
